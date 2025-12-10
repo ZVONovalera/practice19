242d869118d1b6bb2a88f0ad518971e427bdeb6c
Vovanya <vladimirbruh46@gmal.com>
2025-12-11 00:47:59 +0300

    2dff

--- practice19/vite.config.js
+++ practice19/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/practice19/'  // ← имя твоего репозитория!
+  base: '/practice19/'  // имя твоего репо
 });
